133bf0c1d9ec49d21c8a3208f3e006da4fe7be84
Rahma <eshara@Omars-MacBook-Air.local>
2026-08-18 04:57:02 +0300

    add nutrition target calculations

--- src/utils/calculations.ts
+++ src/utils/calculations.ts
@@ -1,3 +1,11 @@
+import type {
+  NonTrainingGoal,
+  TrainingGoal,
+  ActivityLevel,
+  Pace,
+  NutritionTargets,
+} from "../../types/onboarding";
+
 export function calculateBMI(weight: number, height: number): number {
   const squaredHeight = (height / 100) ** 2;
   const bmi = weight / squaredHeight;
@@ -5,10 +13,10 @@ export function calculateBMI(weight: number, height: number): number {
 }
 
 export function calculateBMR(
+  age: number,
   sex: "male" | "female",
   weight: number,
   height: number,
-  age: number,
 ): number {
   let bmr: number;
   if (sex === "male") {
@@ -20,13 +28,14 @@ export function calculateBMR(
 }
 
 export function calculateTDEE(
-  sex: "male" | "female",
-  weight: number,
-  height: number,
   age: number,
-  activityLevel: "sedentary" | "light" | "moderate" | "very" | "extra",
+  sex: "male" | "female",
+  height: number,
+  weight: number,
+  trainingFrequency: number | undefined,
+  activityLevel: ActivityLevel,
 ): number {
-  const bmr = calculateBMR(sex, weight, height, age);
+  const bmr = calculateBMR(age, sex, weight, height);
   const multipliedNumber =
     activityLevel === "sedentary"
       ? 1.2
@@ -37,43 +46,121 @@ export function calculateTDEE(
           : activityLevel === "very"
             ? 1.725
             : 1.9;
-  const tdee = bmr * multipliedNumber;
+  const trainingCalories = trainingFrequency ? trainingFrequency * 50 : 0;
+  const tdee = bmr * multipliedNumber + trainingCalories;
   return tdee;
 }
 
 export function calculateCalorieTarget(
-  goal:
-    | "lose weight"
-    | "lose fat"
-    | "maintain"
-    | "recompose"
-    | "gain muscle"
-    | "gain weight",
-  sex: "male" | "female",
-  weight: number,
-  height: number,
   age: number,
-  activityLevel: "sedentary" | "light" | "moderate" | "very" | "extra",
-  pace?: "slow" | "moderate" | "fast",
+  sex: "male" | "female",
+  height: number,
+  weight: number,
+  trainingFrequency: number | undefined,
+  goal: TrainingGoal | NonTrainingGoal,
+  activityLevel: ActivityLevel,
+  pace?: Pace,
 ): number {
-  const TDEE = calculateTDEE(sex, weight, height, age, activityLevel);
-  let calorieEstimate = TDEE;
+  const tdee = calculateTDEE(
+    age,
+    sex,
+    height,
+    weight,
+    trainingFrequency,
+    activityLevel,
+  );
+  let calorieEstimate = tdee;
   if (goal === "lose fat" || goal === "lose weight" || goal === "recompose") {
     if (pace === "fast") {
-      calorieEstimate = TDEE - (30 / 100) * TDEE;
+      calorieEstimate = tdee - (30 / 100) * tdee;
     } else if (pace === "moderate") {
-      calorieEstimate = TDEE - (20 / 100) * TDEE;
+      calorieEstimate = tdee - (20 / 100) * tdee;
     } else if (pace === "slow") {
-      calorieEstimate = TDEE - (15 / 100) * TDEE;
+      calorieEstimate = tdee - (15 / 100) * tdee;
     }
   } else if (goal === "gain muscle" || goal === "gain weight") {
     if (pace === "fast") {
-      calorieEstimate = TDEE + (15 / 100) * TDEE;
+      calorieEstimate = tdee + (15 / 100) * tdee;
     } else if (pace === "moderate") {
-      calorieEstimate = TDEE + (10 / 100) * TDEE;
+      calorieEstimate = tdee + (10 / 100) * tdee;
     } else if (pace === "slow") {
-      calorieEstimate = TDEE + (5 / 100) * TDEE;
+      calorieEstimate = tdee + (5 / 100) * tdee;
     }
   }
+
   return calorieEstimate;
+}
+
+export function calculateProtein(
+  weight: number,
+  resistanceTraining: boolean,
+  goal: TrainingGoal | NonTrainingGoal,
+): number {
+  const proteinMultiplier =
+    resistanceTraining &&
+    (goal === "lose fat" || goal === "gain muscle" || goal === "recompose")
+      ? 2
+      : resistanceTraining &&
+          (goal === "gain weight" ||
+            goal === "lose weight" ||
+            goal === "maintain")
+        ? 1.6
+        : !resistanceTraining && goal === "lose weight"
+          ? 1.2
+          : 1;
+  const proteinGrams = proteinMultiplier * weight;
+  return proteinGrams;
+}
+
+export function calculateFat(
+  weight: number,
+  resistanceTraining: boolean,
+  goal: TrainingGoal | NonTrainingGoal,
+): number {
+  const fatMultiplier =
+    goal === "lose weight" ||
+    (resistanceTraining && (goal === "lose fat" || goal === "recompose"))
+      ? 0.7
+      : goal === "maintain" || (resistanceTraining && goal === "gain muscle")
+        ? 1
+        : 1.2;
+  const fatGrams = fatMultiplier * weight;
+  return fatGrams;
+}
+
+export function calculateNutritionTargets(
+  age: number,
+  sex: "male" | "female",
+  height: number,
+  weight: number,
+  resistanceTraining: boolean,
+  trainingFrequency: number | undefined,
+  goal: TrainingGoal,
+  activityLevel: ActivityLevel,
+  pace?: Pace,
+): NutritionTargets {
+  const calories = calculateCalorieTarget(
+    age,
+    sex,
+    height,
+    weight,
+    trainingFrequency,
+    goal,
+    activityLevel,
+    pace,
+  );
+  const protein = calculateProtein(weight, resistanceTraining, goal);
+  const fat = calculateFat(weight, resistanceTraining, goal);
+
+  const proteinCalories = protein * 4;
+  const fatCalories = fat * 9;
+
+  const carbs = (calories - proteinCalories - fatCalories) / 4;
+
+  return {
+    calories,
+    protein,
+    fat,
+    carbs,
+  };
 }
--- types/onboarding.ts
+++ types/onboarding.ts
@@ -1,15 +1,48 @@
-export type OnboardingData = {
-  age: number;
-  sex: "male" | "female";
-  height: number;
-  weight: number;
-  goal:
-    | "lose weight"
-    | "lose fat"
-    | "maintain"
-    | "recompose"
-    | "gain muscle"
-    | "gain weight";
-  activityLevel: "sedentary" | "light" | "moderate" | "very" | "extra";
-  pace?: "slow" | "moderate" | "fast";
+export type NonTrainingGoal = "lose weight" | "maintain" | "gain weight";
+
+export type TrainingGoal =
+  | "lose weight"
+  | "lose fat"
+  | "maintain"
+  | "recompose"
+  | "gain muscle"
+  | "gain weight";
+
+export type ActivityLevel =
+  | "sedentary"
+  | "light"
+  | "moderate"
+  | "very"
+  | "extra";
+
+export type Pace = "slow" | "moderate" | "fast";
+
+export type OnboardingData =
+  | {
+      age: number;
+      sex: "male" | "female";
+      height: number;
+      weight: number;
+      resistanceTraining: true;
+      trainingFrequency: number;
+      goal: TrainingGoal;
+      activityLevel: ActivityLevel;
+      pace?: Pace;
+    }
+  | {
+      age: number;
+      sex: "male" | "female";
+      height: number;
+      weight: number;
+      resistanceTraining: false;
+      goal: NonTrainingGoal;
+      activityLevel: ActivityLevel;
+      pace?: Pace;
+    };
+
+export type NutritionTargets = {
+  calories: number;
+  protein: number;
+  fat: number;
+  carbs: number;
 };
